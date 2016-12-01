4899bec8e1603dcc42a1f3710db64e68c97a41a4
Nattiliana <nattiliana@gmail.com>
2016-12-01 15:33:43 +0300

    controller

--- src/by/courses/nattiliana/servlet/Controller.java
+++ src/by/courses/nattiliana/servlet/Controller.java
@@ -12,31 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/controller")
 public class Controller extends HttpServlet {
-  /*  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String page;
-        ActionFactory client = new ActionFactory();
-        ActionCommand command = client.defineCommand(request);
-        page = command.execute(request);
-        if (page != null) {
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(page);
-            dispatcher.forward(request, response);
-        } else {
-            page = ConfigurationManager.getProperty(ConfigConstants.LOGIN_PAGE_PATH);
-            request.getSession().setAttribute("nullPage", MessageManager.getProperty(MessageConstants.PAGE_NOT_FOUND));
-            response.sendRedirect(request.getContextPath() + page);
-        }
-    }*/
-
+ 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String page;
 
